cb6502cefe0006d311d08e32491060afe528164b
ninabreznik <ninabreznik@gmail.com>
2019-07-05 15:14:27 +0100

    Update

--- src/input-boolean.js
+++ src/input-boolean.js
@@ -4,31 +4,33 @@ const validator = require('solidity-validator')
 
 module.exports = displayBooleanInput
 
-function displayBooleanInput ({ theme: { classes: css, colors }, cb }) {
-  const boolFalse = bel `<div class=${css.false} data-state="active" data-type="boolean" value="false" onclick=${toggle}>false</div>`
+function displayBooleanInput ({ theme: { classes: css, colors }, type, cb }) {
+  const boolFalse = bel `<div class=${css.false} data-state="" data-type="boolean" value="false" onclick=${toggle}>false</div>`
   const boolTrue = bel `<div class=${css.true} data-state="" data-type="boolean" value="true" onclick=${toggle}>true</div>`
   const input = bel`<div class=${css.booleanField}>
     ${boolFalse}
     ${boolTrue}
   </div>`
   return input
+
   function toggle (e) {
-    const value = e.target.innerHTML
+    let value = e.target.innerHTML
+    cb(validator.getMessage('boolean', value), e.target, value)
     if (value === 'true') {
-      boolFalse.style.color = colors.whiteSmoke
-      boolFalse.style.backgroundColor = colors.dark
+      boolFalse.style.color = colors.slateGrey
+      boolFalse.style.backgroundColor = colors.darkSmoke
       boolFalse.dataset.state = ""
       boolTrue.dataset.state = "active"
       boolTrue.style.color = colors.dark
       boolTrue.style.backgroundColor = colors.aquaMarine
     } else if (value === 'false') {
-      boolTrue.style.color = colors.whiteSmoke
-      boolTrue.style.backgroundColor = colors.dark
+      boolTrue.style.color = colors.slateGrey
+      boolTrue.style.backgroundColor = colors.darkSmoke
       boolTrue.dataset.state = ""
       boolFalse.dataset.state = "active"
-      boolFalse.style.color = colors.whiteSmoke
+      boolFalse.style.color = colors.dark
       boolFalse.style.backgroundColor = colors.violetRed
     }
-    cb(validator.getMessage('boolean', value), value)
   }
+
 }
